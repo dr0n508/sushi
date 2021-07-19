3475a77c713ff3d3bed0fe53917db2cd8245aada
dr0n <kornienko.andriy@gmail.com>
2021-07-19 16:39:52 +0300

    add filter product template

--- app/js/main.js
+++ app/js/main.js
@@ -96,6 +96,64 @@ $(document).ready(function() {
     });
 
 
+    //tabs
+
+    // $('ul.tabs li').click(function(){
+    //     var tab_id = $(this).attr('data-tab');
+    //
+    //     if ( tab_id === 'tab-all') {
+    //         console.log('all');
+    //         $('.tab-content').addClass('current');
+    //     }
+    //
+    //     else  {
+    //
+    //         $('.tab-content').removeClass('current');
+    //         $("#"+tab_id).addClass('current');
+    //
+    //     }
+    //
+    //     $('ul.tabs li').removeClass('current');
+    //
+    //
+    //     $(this).addClass('current');
+    //
+    // })
+
+    // init Isotope
+    var $grid = $('.grid').isotope({
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows'
+    });
+    // filter functions
+    //     var filterFns = {
+    //         // show if number is greater than 50
+    //         numberGreaterThan50: function() {
+    //             var number = $(this).find('.number').text();
+    //             return parseInt( number, 10 ) > 50;
+    //         },
+    //         // show if name ends with -ium
+    //         ium: function() {
+    //             var name = $(this).find('.name').text();
+    //             return name.match( /ium$/ );
+    //         }
+    //     };
+    // bind filter button click
+        $('.filters-button-group').on( 'click', 'button', function() {
+            var filterValue = $( this ).attr('data-filter');
+            // use filterFn if matches value
+            filterValue = filterFns[ filterValue ] || filterValue;
+            $grid.isotope({ filter: filterValue });
+        });
+    // change is-checked class on buttons
+        $('.button-group').each( function( i, buttonGroup ) {
+            var $buttonGroup = $( buttonGroup );
+            $buttonGroup.on( 'click', 'button', function() {
+                $buttonGroup.find('.is-checked').removeClass('is-checked');
+                $( this ).addClass('is-checked');
+            });
+        });
+
 
 });
 
