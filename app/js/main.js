a9b279d041f695ddc739886ca3abb9768c5b1ec5
dr0n <kornienko.andriy@gmail.com>
2021-07-19 17:10:49 +0300

    add filter product template

--- app/js/main.js
+++ app/js/main.js
@@ -121,23 +121,23 @@ $(document).ready(function() {
     // })
 
     // init Isotope
-    var $grid = $('.grid').isotope({
-        itemSelector: '.element-item',
+    var $grid = $('.grid-filter').isotope({
+        itemSelector: '.product-item',
         layoutMode: 'fitRows'
     });
     // filter functions
-    //     var filterFns = {
-    //         // show if number is greater than 50
-    //         numberGreaterThan50: function() {
-    //             var number = $(this).find('.number').text();
-    //             return parseInt( number, 10 ) > 50;
-    //         },
-    //         // show if name ends with -ium
-    //         ium: function() {
-    //             var name = $(this).find('.name').text();
-    //             return name.match( /ium$/ );
-    //         }
-    //     };
+        var filterFns = {
+            // show if number is greater than 50
+            numberGreaterThan50: function() {
+                var number = $(this).find('.number').text();
+                return parseInt( number, 10 ) > 50;
+            },
+            // show if name ends with -ium
+            ium: function() {
+                var name = $(this).find('.name').text();
+                return name.match( /ium$/ );
+            }
+        };
     // bind filter button click
         $('.filters-button-group').on( 'click', 'button', function() {
             var filterValue = $( this ).attr('data-filter');
